cd0b1e7c63e07b83e20847c4fdeb00ca9b6a7abd
Djordje Krstic <krsticdjordje372@gmail.com>
2025-07-19 19:39:16 +0200

    Started practice fro adapter pattern. Created abstract Logger class and LoggerImpl class and added commons-logger dependency.

---
+++ src/main/java/org/example/adapter_pattern/Logger.java
@@ -0,0 +1,13 @@
+package org.example.adapter_pattern;
+
+public abstract class Logger {
+
+    abstract public void trace(String message);
+    abstract public void debug(String message);
+    abstract public void log(String message);
+
+    public static Logger create() {
+        return new LoggerImpl();
+    }
+
+}
---
+++ src/main/java/org/example/adapter_pattern/LoggerImpl.java
@@ -0,0 +1,18 @@
+package org.example.adapter_pattern;
+
+public class LoggerImpl extends Logger {
+    @Override
+    public void trace(String message) {
+        System.out.println("[TRACE] " + message);
+    }
+
+    @Override
+    public void debug(String message) {
+        System.out.println("[DEBUG] " + message);
+    }
+
+    @Override
+    public void log(String message) {
+        System.out.println("[LOG] " + message);
+    }
+}
