5498d044592dec6aaf0dc230607de85cc32e9b26
Djordje Krstic <krsticdjordje372@gmail.com>
2025-07-20 19:10:46 +0200

    Created a method in Main clas that writes out a sample text in a text file using output stream.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,32 @@ package org.example;
 
 import org.example.proxy_pattern.BusinessObject;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+
 public class Main {
     public static void main(String[] args) {
 //        Proxy pattern
 //        BusinessObject businessObject = BusinessObject.create();
 //        businessObject.sayHi();
 
+        try {
+            OutputStream os = new FileOutputStream("test.txt");
+            writeSomeContentToAnOutputStream(os);
+            os.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    public static void writeSomeContentToAnOutputStream(OutputStream os) throws IOException {
+        String someContent = "This is a test";
+        os.write(someContent.getBytes(Charset.forName("UTF-8")));
     }
 }
